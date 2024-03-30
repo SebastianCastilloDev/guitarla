6ebfd17d932dda8e68aeb47639276dceabdc887d
Sebastian Castillo <tantrum5535@gmail.com>
2024-03-30 09:22:32 -0300

    Mostrando la informacion de cada guitarra

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState, useEffect } from "react"
-import Header from "./components/Header"
-import Guitar from "./components/Guitar"
-import { db } from "./data/db"
-
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setData] = useState(db);
 
-  const [data, setData] = useState([])
-
-  useEffect(()=>{
-    setData(db)
-  },[])
-
-  return(
+  return (
     <>
       <Header />
       <main className="container-xl mt-5">
-          <h2 className="text-center">Nuestra Colección</h2>
-            
-          <div className="row mt-5">
-            <Guitar />
-          </div>
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} />
+          ))}
+        </div>
       </main>
       <footer className="bg-dark mt-5 py-5">
-          <div className="container-xl">
-              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-          </div>
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
