c45954f6ebd3f576705cee471ea8ffdd851af59a
Sebastian Castillo <tantrum5535@gmail.com>
2024-04-05 00:32:56 -0300

    agregando el state en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { db } from "./data/db";
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
   return (
     <>
@@ -14,7 +15,7 @@ function App() {
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar key={guitar.id} guitar={guitar} setCart={setCart} />
           ))}
         </div>
       </main>
